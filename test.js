91dd948253c85f5a545dfb0f3388481cd327c2b2
Rosantex <1468914689@naver.com>
2017-05-27 16:54:36 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,20 +1,15 @@
 // Legacy's stock
 
 Array.prototype.random = function() {
-        return this[Math.floor(Math.random() * this.length)];
-    };
+    return this[Math.floor(Math.random() * this.length)];
+};
        
-    Array.prototype.shuffle = function() {
-        return this.sort(function() {
-            return Math.random() - 0.5;
-        });
-    };
-    Function.prototype.__extends = function(parent) {
-        this.prototype = Object.create(parent.prototype);
-    };
+Array.prototype.shuffle = function() {
+    return this.sort(function() {
+       return Math.random() - 0.5;
+    });
+};
 
-    var T = setTimeout;
-
-    function R(x, y) {
-        return y ? x + Math.floor(Math.random() * (y - x + 1)) : R(0, x);
-    }
+Function.prototype.__extends = function(parent) {
+    this.prototype = Object.create(parent.prototype);
+};
