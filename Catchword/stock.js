2912a494e0cf6e83551540dfa29fd03bec240bda
Rosantex <1468914689@naver.com>
2017-05-29 17:07:40 +0900

    Update and rename prototype.js to stock.js

--- Catchword/stock.js
+++ Catchword/stock.js
@@ -8,8 +8,16 @@ Array.prototype.shuffle = function() {
     return this.sort(function() { 
         return Math.random() - 0.5; 
     });
-}; 
+};
 
 Function.prototype.__extends = function(superClass) { 
     this.prototype = Object.create(superClass.prototype);
 }; 
+
+Object._entries = function(obj) {
+    var entry = [];
+    for (key in obj)
+        entry[entry.length] = [key, obj[key]];
+    
+    return entry;
+};
